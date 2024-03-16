0a0046c363fef009eef7f0516be7c31324b2906e
Nabidul Islam <Nabidulislam90@gmail.com>
2024-03-16 22:54:37 +1100

    Grade_Tracker_Version_2

--- Student_Grade_Tracker_Project/Student_Grade_Tracker.java
+++ Student_Grade_Tracker_Project/Student_Grade_Tracker.java
@@ -1,13 +1,33 @@
 package Student_Grade_Tracker_Project;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Student_Grade_Tracker{
-    public int selected_option(){
+    public void sem_subjects(){
         Scanner sc = new Scanner(System.in);
-        int user_choice = sc.nextInt();
-        System.out.println("Your seleted option is: " + user_choice);
+        Map<String, Integer> subjectDetails = new HashMap<>();
+        System.out.println("How many courses are you doing this semester?");
+        int numSubject = sc.nextInt();
+        System.out.println("Enter your course name & credit points it is. If it's a pass/fail course enter 0 credit points.");
+        for(int i = 0; i < numSubject; i++){
+            System.out.println("Enter the name of course " + (i+1) + ":");
+            String subjectName = sc.nextLine();
+            System.out.println("Enter the credit points for " + subjectName + ":");
+            int subjectCredit = sc.nextInt();
+            subjectDetails.put(subjectName, subjectCredit);
+        }
         sc.close();
-        return user_choice;
+    }
+    
+    public void selected_option(){
+        Scanner sc = new Scanner(System.in);
+        int userChoice = sc.nextInt();
+        System.out.println("Your seleted option is: " + userChoice);
+        if(userChoice == 1){
+            sem_subjects();
+        }
+        sc.close();
     }
 
     public void user_menu(){
@@ -16,7 +36,6 @@ public class Student_Grade_Tracker{
         System.out.println("Option 2: Load existing student report");
         selected_option();
     }
-
     public static void main(String[] args) {
         System.out.println("Welcome to you student grade tracker!");
         Student_Grade_Tracker record = new Student_Grade_Tracker();
